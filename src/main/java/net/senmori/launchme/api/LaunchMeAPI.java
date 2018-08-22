8ccda05227c917abe514b0e8331d3ec220eadfcd
Senmori <thesenmori@gmail.com>
2018-08-22 19:28:24 -0400

    Update LaunchMeAPI javadoc

--- src/main/java/net/senmori/launchme/api/LaunchMeAPI.java
+++ src/main/java/net/senmori/launchme/api/LaunchMeAPI.java
@@ -2,5 +2,11 @@ package net.senmori.launchme.api;
 
 import net.senmori.launchme.api.manager.*;
 
+/**
+ * A central interface that simply extends all other interfaces that are
+ * public-facing. <br>
+ * This is simply a convenient replacement for retrieving each service
+ * individually.
+ */
 public interface LaunchMeAPI extends TransportManager, TargetManager, TransportMethodManager, TransportOptionsManager, TransportTypeManager {
 }
